5be571c6e15ffee968562126c4c754e2945f00f4
Matt Gibson <mattgibson333@gmail.com>
2024-11-07 12:37:35 -0500

    Added comments

--- backend/backend.py
+++ backend/backend.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# Gets the current time of day and displays it
 @app.route('/api/time', methods=['GET'])
 def get_current_time():
     return {'time': time.time()}
 
+# Gets the current day of the year
 @app.route('/api/date', methods=['POST'])
 def getGamesForDate():
     #date must be in format: '07/31/2018'
