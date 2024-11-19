6b96715b1eb553c9265847e095ad4d0125f0a8ec
Matt Gibson <mattgibson333@gmail.com>
2024-11-19 14:05:29 -0500

    changes

--- backend/mlAdapter.py
+++ backend/mlAdapter.py
@@ -4,32 +4,27 @@ import pandas as pd
 from MLB_Statistics import getTeamStats
 import models
 import numpy as np
-
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.preprocessing import OneHotEncoder
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-# import requests
-# import duckdb
-# #import ml_insights as mli
-# import warnings
-# warnings.filterwarnings("ignore")
-# import matplotlib.pyplot as plt
-# import lightgbm as lgbm
-# #import ml_insights as mli
-
-# Load the pkl model
-with open('win_analysis.pkl', 'rb') as file:
-    winModel_pkl = pickle.load(file)
+import lightgbm as lgbm
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import duckdb
+import ml_insights as mli
+import warnings
+warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
 
 
-# Function to create a DataFrame and get predictions from the pkl model
+winModel_pkl = joblib.load('backend\models\win_analysis.pkl')
+
+# Function to create a DataFrame and get predictions
 def get_win_prediction(home_team, visiting_team, season):
-    # Retrieve team stats for both home and visiting teams
+    # Retrieve team stats
     home_stats = getTeamStats(home_team, season)
     visiting_stats = getTeamStats(visiting_team, season)
 
-    # Ensure all required stats are valid before making a prediction
+    # Ensure stats are valid
     if home_stats['OBP'] != 'N/A' and visiting_stats['OBP'] != 'N/A' and \
        home_stats['SLG'] != 'N/A' and visiting_stats['SLG'] != 'N/A':
 
@@ -41,11 +36,11 @@ def get_win_prediction(home_team, visiting_team, season):
             visiting_stats['SLG']
         ]], columns=['OBP_162_h', 'OBP_162_v', 'SLG_162_h', 'SLG_162_v'])
 
-        # Make a prediction (probability of the home team winning)
-        prediction_pkl = winModel_pkl.predict_proba(input_data)[:, 1]
+        # Make predictions
+        prediction_pkl = winModel_pkl.predict_proba(input_data)[:, 1]  # Probability of home team winning
 
         return {
-            "pkl_model_prediction": prediction_pkl[0]  # Return the probability of the home team winning
+            "pkl_model_prediction": prediction_pkl[0]
         }
     else:
         return "One or more statistics were not available. Prediction could not be made."
@@ -58,5 +53,6 @@ season = "2023"
 prediction = get_win_prediction(home_team, visiting_team, season)
 if isinstance(prediction, dict):
     print(f"Probability of {home_team} winning (pkl model): {prediction['pkl_model_prediction']:.2%}")
+    print(f"Probability of {home_team} winning (joblib model): {prediction['joblib_model_prediction']:.2%}")
 else:
     print(prediction)
