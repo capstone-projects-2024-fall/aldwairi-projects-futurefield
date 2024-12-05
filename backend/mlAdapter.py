ba3b98101c52e8e8ff05a13479500f340fe8c6bd
Matt Gibson <mattgibson333@gmail.com>
2024-12-04 21:20:14 -0500

    Deleted repeat code

--- backend/mlAdapter.py
+++ backend/mlAdapter.py
@@ -5,13 +5,9 @@ from backend.MLB_Statistics import getTeamStats
 import warnings
 warnings.filterwarnings("ignore")
 
-# Load the pitch prediction model
-pitch_model = joblib.load('backend\models\pitchType_API_RandomForrest.joblib')
 
 # Load the win prediction model
 winModel_pkl = joblib.load('backend\models\win_analysis.pkl')
-
-
 
 
 # Function to create a DataFrame and get predictions for winModel_pkl
